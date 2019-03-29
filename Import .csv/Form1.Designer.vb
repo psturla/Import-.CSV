5682c1fdd1fbd5db344a894ed42382abb9eb062c
Pablo Sturla <stu.pablo@gmail.com>
2019-03-29 13:31:52 -0300

    Import .csv

---
+++ Import .csv/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.grdCsv = New System.Windows.Forms.DataGridView()
+        Me.btnOpenFile = New System.Windows.Forms.Button()
+        Me.txtFile = New System.Windows.Forms.TextBox()
+        CType(Me.grdCsv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'grdCsv
+        '
+        Me.grdCsv.AllowUserToAddRows = False
+        Me.grdCsv.AllowUserToDeleteRows = False
+        Me.grdCsv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdCsv.Location = New System.Drawing.Point(13, 54)
+        Me.grdCsv.Name = "grdCsv"
+        Me.grdCsv.ReadOnly = True
+        Me.grdCsv.Size = New System.Drawing.Size(595, 395)
+        Me.grdCsv.TabIndex = 0
+        '
+        'btnOpenFile
+        '
+        Me.btnOpenFile.Location = New System.Drawing.Point(13, 12)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(126, 36)
+        Me.btnOpenFile.TabIndex = 1
+        Me.btnOpenFile.Text = "Open .CSV"
+        Me.btnOpenFile.UseVisualStyleBackColor = True
+        '
+        'txtFile
+        '
+        Me.txtFile.Location = New System.Drawing.Point(145, 12)
+        Me.txtFile.Multiline = True
+        Me.txtFile.Name = "txtFile"
+        Me.txtFile.Size = New System.Drawing.Size(200, 36)
+        Me.txtFile.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(644, 486)
+        Me.Controls.Add(Me.txtFile)
+        Me.Controls.Add(Me.btnOpenFile)
+        Me.Controls.Add(Me.grdCsv)
+        Me.Name = "Form1"
+        Me.Text = "Open .CSV"
+        CType(Me.grdCsv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents grdCsv As DataGridView
+    Friend WithEvents btnOpenFile As Button
+    Friend WithEvents txtFile As TextBox
+End Class
---
+++ Import .csv/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        txtFile.Enabled = False
+
+    End Sub
+
+    Private Sub btnOpenFile_Click(sender As Object, e As EventArgs) Handles btnOpenFile.Click
+
+        'Traer directorio
+        Dim OpenFileDialog1 As New OpenFileDialog
+        OpenFileDialog1.InitialDirectory = "C:\"
+        OpenFileDialog1.FileName = ""
+        OpenFileDialog1.Multiselect = False
+
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim sName As String = OpenFileDialog1.SafeFileName
+            txtFile.Text = OpenFileDialog1.FileName
+
+            'Dim directory As String = System.Configuration.ConfigurationManager.AppSettings.Get("Dir")
+
+            ' Import CSV
+            Dim TextFileReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(txtFile.Text)
+
+            TextFileReader.TextFieldType = FileIO.FieldType.Delimited
+            TextFileReader.SetDelimiters(ChrW(1))
+
+            Dim TextFileTable As DataTable = Nothing
+
+            Dim Column As DataColumn
+            Dim Row As DataRow
+            Dim UpperBound As Int32
+            Dim ColumnCount As Int32
+            Dim CurrentRow As String()
+            Dim firstRow As Boolean = True
+
+            While Not TextFileReader.EndOfData
+                Try
+                    CurrentRow = TextFileReader.ReadFields()
+                    If Not CurrentRow Is Nothing Then
+                        If TextFileTable Is Nothing Then
+                            TextFileTable = New DataTable("TextFileTable")
+                            UpperBound = CurrentRow.GetUpperBound(0)
+                            For ColumnCount = 0 To UpperBound
+                                Column = New DataColumn()
+                                Column.DataType = System.Type.GetType("System.String")
+                                Column.ColumnName = CurrentRow(ColumnCount).ToString
+                                Column.Caption = CurrentRow(ColumnCount).ToString
+                                Column.ReadOnly = True
+                                Column.Unique = False
+                                TextFileTable.Columns.Add(Column)
+                            Next
+                        End If
+                        Row = TextFileTable.NewRow
+                        If Not firstRow Then
+                            For ColumnCount = 0 To UpperBound
+                                Row(ColumnCount) = CurrentRow(ColumnCount).ToString
+                            Next
+                            TextFileTable.Rows.Add(Row)
+                        End If
+                        firstRow = False
+                    End If
+                Catch ex As _
+                Microsoft.VisualBasic.FileIO.MalformedLineException
+                    MsgBox("Error en la linea: " & ex.Message)
+                End Try
+            End While
+            TextFileReader.Dispose()
+            grdCsv.DataSource = TextFileTable
+
+        End If
+
+
+    End Sub
+End Class
